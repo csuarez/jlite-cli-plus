4970c089c42a6c48821c8c5262ddacbf80e44a2d
César Suárez <suarez.ortega.cesar@gmail.com>
2011-06-21 21:14:06 +0200

    XML output for proxy-init

--- src/jlite/cli/ProxyInit.java
+++ src/jlite/cli/ProxyInit.java
@@ -49,20 +49,27 @@ public class ProxyInit {
 	    Options options = setupOptions();
 	    HelpFormatter helpFormatter = new HelpFormatter();
 	    helpFormatter.setSyntaxPrefix("Usage: ");
+	    CommandLine line = null;
 		try {
-			CommandLine line = parser.parse(options, args);			
+			line = parser.parse(options, args);			
             if (line.hasOption("help")) {
         		System.out.println(); // extra line
                 helpFormatter.printHelp(100, COMMAND, "\noptions:", options, "\n"+CLI.FOOTER+"\n", false);
                 System.out.println(); // extra line
                 System.exit(0);
             } else {
+            	if (line.hasOption("xml")) {
+					System.out.println("<output>");
+				}
             	String[] remArgs = line.getArgs();
             	if (remArgs.length > 0) {
             		run(remArgs, line);
             	} else {
             		throw new MissingArgumentException("Missing required argument: <voms>[:<command>]");
             	}
+            	if (line.hasOption("xml")) {
+					System.out.println("</output>");
+				}
             }
 		} catch (ParseException e) {
             System.err.println("\n" + e.getMessage() + "\n");
@@ -70,7 +77,11 @@ public class ProxyInit {
             System.out.println(); // extra line
             System.exit(-1);
 		} catch (Exception e) {
-			System.err.println(e.getMessage());
+			if ((line != null) && (line.hasOption("xml"))) {
+				System.out.println("<error>" + e.getMessage() + "</error>");
+			} else {
+				System.err.println(e.getMessage());
+			}
 		}
 		System.out.println(); // extra line
 	}
@@ -123,6 +134,11 @@ public class ProxyInit {
                 .withDescription("set your private key passphrase without any prompt")
                 .hasArg()
                 .create("password"));
+
+        options.addOption(OptionBuilder
+        		.withArgName("xml")
+                .withDescription("output as xml")
+                .create("xml"));
         
         return options;
 	}
@@ -167,12 +183,26 @@ public class ProxyInit {
 		boolean limited = line.hasOption("limited");
 
 		GlobusCredential proxy = grid.createProxy(vomsArgs, lifetime, proxyType, limited);
-		
-		System.out.println("Created VOMS proxy: " + proxy.getSubject());
+
+		if (line.hasOption("xml")) {
+			System.out.println("<voms-proxy>" + proxy.getSubject() + "</voms-proxy>");
+		} else {
+			System.out.println("Created VOMS proxy: " + proxy.getSubject());
+		}
+
 		long expiryTime = System.currentTimeMillis() + proxy.getTimeLeft()*1000;
-		SimpleDateFormat df = new SimpleDateFormat("EEE MMM d HH:mm:ss yyyy z", Locale.ENGLISH);				
-		System.out.println("Proxy is valid until: " + df.format(new Date(expiryTime)));
-		System.out.println("Proxy location: " + new File(conf.getProxyPath()).getAbsolutePath());
+		SimpleDateFormat df = new SimpleDateFormat("EEE MMM d HH:mm:ss yyyy z", Locale.ENGLISH);
+		if (line.hasOption("xml")) {
+			System.out.println("<valid-until>" + df.format(new Date(expiryTime)) + "</valid-until>");
+		} else {				
+			System.out.println("Proxy is valid until: " + df.format(new Date(expiryTime)));
+		}
+
+		if (line.hasOption("xml")) {
+			System.out.println("<proxy-location>" + new File(conf.getProxyPath()).getAbsolutePath() + "</proxy-location>");
+		} else {
+			System.out.println("Proxy location: " + new File(conf.getProxyPath()).getAbsolutePath());
+		}
 	}
 	
 }
