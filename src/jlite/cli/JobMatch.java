1a12c75faf4fa4c8c4686dddb359b49c4f20895e
César Suárez Ortega <suarez.ortega.cesar@gmail.com>
2011-07-04 18:37:19 +0200

    XML output for job-match.

--- src/jlite/cli/JobMatch.java
+++ src/jlite/cli/JobMatch.java
@@ -46,13 +46,17 @@ public class JobMatch {
 	    Options options = setupOptions();
 	    HelpFormatter helpFormatter = new HelpFormatter();
 	    helpFormatter.setSyntaxPrefix("Usage: ");
+        CommandLine line = null;
 		try {
-			CommandLine line = parser.parse(options, args);
+			line = parser.parse(options, args);
             if (line.hasOption("help")) {                
             	helpFormatter.printHelp(100, COMMAND, "\noptions:", options, "\n"+CLI.FOOTER, false);
         		System.out.println(); // extra line
                 System.exit(0);
             } else {
+                if (line.hasOption("xml")) {
+                    System.out.println("<output>");
+                }
             	String[] remArgs = line.getArgs();
             	if (remArgs.length == 1) {
             		run(remArgs[0], line);
@@ -68,8 +72,16 @@ public class JobMatch {
     		System.out.println(); // extra line
             System.exit(-1);
 		} catch (Exception e) {
-			System.err.println(e.getMessage());
-		}
+            if (line.hasOption("xml")) {
+                System.out.println("<error>" + e.getMessage() + "</error>");
+            } else {
+                System.err.println(e.getMessage());
+            }
+		} finally {
+            if (line.hasOption("xml")) {
+                System.out.println("</output>");
+            }      
+        }
 		System.out.println(); // extra line
 	}
 
@@ -101,6 +113,11 @@ public class JobMatch {
                 .withDescription("non-standard location of proxy cert")
                 .hasArg()
                 .create("proxypath"));
+
+        options.addOption(OptionBuilder
+                .withArgName("xml")
+                .withDescription("output as xml")
+                .create("xml"));
         
         return options;
 	}
@@ -114,7 +131,11 @@ public class JobMatch {
         }
 
 		String vo = Util.readVOFromVOMSProxy(conf.getProxyPath());
-		System.out.println("Working VO: " + vo);
+        if (line.hasOption("xml")) {
+            System.out.println("<vo>" + vo + "</vo>");
+		} else {
+            System.out.println("Working VO: " + vo);
+        }
 		String wmProxyURL = conf.getWMProxies().get(vo);
 		if (line.hasOption("e")) {
 			wmProxyURL = line.getOptionValue("e");
@@ -122,15 +143,22 @@ public class JobMatch {
 		if (wmProxyURL == null) {
 			throw new GridAPIException("Could not find WMProxy server for VO: " + vo);
 		}
-		System.out.println("Connecting to WMProxy service: " + wmProxyURL + "\n");
-	
+        if (line.hasOption("xml")) {
+            System.out.println("<wmProxy>" + wmProxyURL + "</wmProxy>");
+        } else {
+		      System.out.println("Connecting to WMProxy service: " + wmProxyURL + "\n");
+	    }
 		String delegationId;
 		if (line.hasOption("a")) {
 			grid = GridSessionFactory.create(conf);			
 			delegationId = line.getOptionValue("d", System.getProperty("user.name"));	
-			grid.delegateProxy(wmProxyURL, delegationId);			
-			System.out.println("Your proxy has been successfully delegated to WMProxy\n" + 
+			grid.delegateProxy(wmProxyURL, delegationId);	
+            if (line.hasOption("xml")) {
+                System.out.println("<delegationId>" + delegationId + "</delegationId>");
+            } else {		
+			     System.out.println("Your proxy has been successfully delegated to WMProxy\n" + 
 					"Delegation identifier: " + delegationId + "\n");
+            }
 		} else {
 			delegationId = line.getOptionValue("d", System.getProperty("user.name"));
 			conf.setDelegationId(delegationId);
@@ -142,20 +170,41 @@ public class JobMatch {
 		
         List<MatchedCE> ces = grid.listMatchedCE(wmProxyURL, jad.toString());
         
-        System.out.println("Found " + ces.size() + " CE(s) matching your job requirements: ");
+//        System.out.println("Found " + ces.size() + " CE(s) matching your job requirements: ");
         int longestCeId = 0;
         for (MatchedCE ce : ces) {
         	if (ce.getId().length() > longestCeId) {
         		longestCeId = ce.getId().length();
         	}
         }
+        if (line.hasOption("xml")) {
+            System.out.println("<matchedCEs>");
+        }
         int rankPos = longestCeId + 10;
         for (MatchedCE ce : ces) {
-        	System.out.print("\t" + ce.getId());
-        	for (int i=0; i < rankPos - ce.getId().length(); i++) {
-        		System.out.print(" ");
-        	}
-        	System.out.print(ce.getRank() + "\n");
+            if (line.hasOption("xml")) {
+                System.out.println("<ce>");
+            }
+            if (line.hasOption("xml")) {
+                System.out.println("<id>" + ce.getId() + "</id>");
+            } else {
+        	   System.out.print("\t" + ce.getId());
+               for (int i=0; i < rankPos - ce.getId().length(); i++) {
+                    System.out.print(" ");
+                }
+            }
+
+        	if (line.hasOption("xml")) {
+                System.out.println("<rank>" + ce.getRank() + "</rank>");
+            } else {
+        	   System.out.print(ce.getRank() + "\n");
+            }
+            if (line.hasOption("xml")) {
+                System.out.println("</ce>");
+            }
+        }
+        if (line.hasOption("xml")) {
+            System.out.println("</matchedCEs>");
         }
 	}
 
